94869c659d2bbae45d452282eef9f6789c70a92f
Brian Holestine <holestine@gmail.com>
2023-09-21 09:52:48 -0700

    cleanup

--- tools/uniad_create_data.sh
+++ tools/uniad_create_data.sh
@@ -1,6 +1,7 @@
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python tools/create_data.py nuscenes --root-path ./data/nuscenes \
+python tools/create_data.py nuscenes \
+       --root-path ./data/nuscenes \
        --out-dir ./data/infos \
        --extra-tag nuscenes \
        --version v1.0 \
